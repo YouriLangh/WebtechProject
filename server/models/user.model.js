7b05bbc869365bd30b1c8399ab5a18619cdbb2be
louivers <Louis.Verstraeten@vub.be>
2022-12-17 22:50:14 +0100

    merge and yarn install

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -12,7 +12,7 @@ const User = new mongoose.Schema({
     url: {type: String, required: true, default: 'pfp/default_pfp'},
     interests: [{type: String}],
     comments: [CommentSchema],
-    rating: {type: Number}
+    rating: {type: Number},
     activities: [{type: String}]
 },
 { collection: 'user-data'}
